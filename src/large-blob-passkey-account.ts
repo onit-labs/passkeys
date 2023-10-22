b6a438808f2cab0c9ba5038ac82915df4af7511a
Peter Ferguson <peterferguson95@gmail.com>
2023-10-22 19:36:50 +0100

    wip: skeleton of cf account with 6492

--- src/large-blob-passkey-account.ts
+++ src/large-blob-passkey-account.ts
@@ -4,15 +4,13 @@ import {
 	signTransaction,
 	signTypedData,
 	toAccount,
-} from 'viem/accounts'
-import { base64URLStringToHex } from './utils/encoding'
+} from "viem/accounts";
+import { base64URLStringToHex } from "./utils/encoding";
 
-import type { Address, Hash, Hex, PrivateKeyAccount } from 'viem'
-import type { AuthenticationResponseJSON } from './passkey.types'
-import { Passkey } from './passkey'
-
-// - Branded types to make it clearer what to pass
-type Base64URLString = string
+import type { Account, Address, Hash, Hex, PrivateKeyAccount } from "viem";
+import type { AuthenticationResponseJSON } from "./passkey.types";
+import { Passkey } from "./passkey";
+import { Base64URLString } from "./utils/webauthn-zod";
 
 /**
  * Allow user to initiate the account having already verified the user holds the passkey
@@ -21,120 +19,121 @@ type Base64URLString = string
  * Or allow the user to delay initiation of the account until sign time
  */
 type Params<TPasskey extends Passkey> = { passkey: TPasskey } & PresentationParams &
-	({ address: Address; credentialId: Base64URLString } | { privateKey: Hash })
+	({ address: Address; credentialId: Base64URLString } | { privateKey: Hash });
 
 /**
  * Imperative methods to present the end user with UI that will explain the transaction/message
  * This is an attempt to force developers to MAKE SURE that end users are not blind signing with their passkey
  */
 export interface PresentationParams {
-	presentSignMessage: (message: Parameters<typeof signMessage>[0]['message']) => Promise<boolean>
+	presentSignMessage: (message: Parameters<typeof signMessage>[0]["message"]) => Promise<boolean>;
 	presentSignTransaction: (
-		transaction: Parameters<typeof signTransaction>[0]['transaction'],
-	) => Promise<boolean>
+		transaction: Parameters<typeof signTransaction>[0]["transaction"],
+	) => Promise<boolean>;
 	presentSignTypedData: (
-		typedData: Omit<Parameters<typeof signTypedData>[0], 'privateKey'>,
-	) => Promise<boolean>
+		typedData: Omit<Parameters<typeof signTypedData>[0], "privateKey">,
+	) => Promise<boolean>;
 }
 
 export class LargeBlobPasskeyAccount<TPasskey extends Passkey = Passkey> {
-	address?: Address
-	credentialId?: Base64URLString
-	privateKey?: Hash
-	context: PresentationParams
-	passkey: TPasskey
+	address?: Address;
+	credentialId?: Base64URLString;
+	privateKey?: Hash;
+	context: PresentationParams;
+	passkey: TPasskey;
 
 	constructor(params: Params<TPasskey>) {
-		this.passkey = params.passkey
+		this.passkey = params.passkey;
 		this.context = {
 			presentSignMessage: params.presentSignMessage,
 			presentSignTransaction: params.presentSignTransaction,
 			presentSignTypedData: params.presentSignTypedData,
+		};
+
+		if ("address" in params) {
+			this.address = params.address;
+			this.credentialId = params.credentialId;
 		}
 
-		if ('address' in params) {
-			this.address = params.address
-			this.credentialId = params.credentialId
-		}
-
-		if ('privateKey' in params) {
-			this.privateKey = params.privateKey
+		if ("privateKey" in params) {
+			this.privateKey = params.privateKey;
 		}
 	}
 
 	initPrivateKeyFromPasskey(
-		authenticationResponse: Pick<AuthenticationResponseJSON, 'clientExtensionResults'>,
+		authenticationResponse: Pick<AuthenticationResponseJSON, "clientExtensionResults">,
 	): Hex {
-		const largeBlob = authenticationResponse?.clientExtensionResults.largeBlob?.blob
+		const largeBlob = authenticationResponse?.clientExtensionResults.largeBlob?.blob;
 
 		// TODO: should we encode the blob as JSON or just assume it is actually the privateKey?
 
 		if (!largeBlob) {
-			throw new Error('No blob stored on passkey')
+			throw new Error("No blob stored on passkey");
 		}
 
 		// ! for now assuming that the blob is JUST a privateKey
-		this.privateKey = base64URLStringToHex(largeBlob)
+		this.privateKey = base64URLStringToHex(largeBlob);
 
-		return this.privateKey
+		return this.privateKey;
 	}
 
 	initAccount() {
 		if (!this.address && this.privateKey) {
-			this.address = privateKeyToAddress(this.privateKey)
+			this.address = privateKeyToAddress(this.privateKey);
 		}
-		if (!this.address) throw new Error('Failed to find address associted to the account')
+		if (!this.address) throw new Error("Failed to find address associted to the account");
 	}
 
 	toAccount(): PrivateKeyAccount {
-		if (!this.address) this.initAccount()
-		const { presentSignMessage, presentSignTransaction, presentSignTypedData } = this.context
+		if (!this.address) this.initAccount();
+		const { presentSignMessage, presentSignTransaction, presentSignTypedData } = this.context;
 
 		const getPrivateKey = async () => {
-			if (this.privateKey) return this.privateKey
+			if (this.privateKey) return this.privateKey;
 
 			// - use passkey largeBlob extension to return the EOA privateKey
-			if (!this.credentialId) throw new Error('No passkey credentialId was found')
+			if (!this.credentialId) throw new Error("No passkey credentialId was found");
 
 			// TODO: if privateKey exists store it and delete from local?
 			// TODO: or create a variable that detects if it has been stored and ensure that it is stored on passkey as well
 
 			// @ts-expect-error: we ignore the challenge in our version
 			const response = await this.passkey.get({
-				allowCredentials: [{ type: 'public-key', id: this.credentialId }],
-			})
+				allowCredentials: [{ type: "public-key", id: this.credentialId }],
+			});
 
-			if (!response) throw new Error('No response from passkey')
-			console.log('toAccount - response', response)
+			if (!response) throw new Error("No response from passkey");
+			console.log("toAccount - response", response);
 
-			return this.initPrivateKeyFromPasskey(response)
-		}
+			return this.initPrivateKeyFromPasskey(response);
+		};
 
 		return {
 			...toAccount({
 				// biome-ignore lint/style/noNonNullAssertion: we throw an error or initialise this
 				address: this.address!, // TODO: add throw expression instead of ignoring?
 				async signMessage({ message }) {
-					if (!(await presentSignMessage(message))) throw new Error('Failed to sign message')
-					console.log('signMessage', message)
-					const privateKey = await getPrivateKey()
-					return signMessage({ message, privateKey })
+					if (!(await presentSignMessage(message))) throw new Error("Failed to sign message");
+					console.log("signMessage", message);
+					const privateKey = await getPrivateKey();
+					return signMessage({ message, privateKey });
 				},
 				async signTransaction(transaction, { serializer } = {}) {
 					if (!(await presentSignTransaction(transaction)))
-						throw new Error('Failed to sign transation')
-					const privateKey = await getPrivateKey()
-					return signTransaction({ privateKey, transaction, serializer })
+						throw new Error("Failed to sign transation");
+					const privateKey = await getPrivateKey();
+					return signTransaction({ privateKey, transaction, serializer });
 				},
 				async signTypedData(typedData) {
-					if (!(await presentSignTypedData(typedData))) throw new Error('Failed to sign typed data')
-					const privateKey = await getPrivateKey()
-					return signTypedData({ ...typedData, privateKey })
+					if (!(await presentSignTypedData(typedData)))
+						throw new Error("Failed to sign typed data");
+					const privateKey = await getPrivateKey();
+					return signTypedData({ ...typedData, privateKey });
 				},
 			}),
-			source: 'privateKey',
-		}
+			source: "privateKey" as const,
+		} satisfies Account;
 	}
 }
 
-export type PasskeyAccount = ReturnType<InstanceType<typeof LargeBlobPasskeyAccount>['toAccount']>
+export type PasskeyAccount = ReturnType<InstanceType<typeof LargeBlobPasskeyAccount>["toAccount"]>;
