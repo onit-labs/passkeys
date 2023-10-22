48c39d55be7246ac3d8fb6ae71b51975c9843591
Peter Ferguson <peterferguson95@gmail.com>
2023-10-22 19:00:36 +0100

    wip: create smart account passkey abstract class

--- src/passkey.ts
+++ src/passkey.ts
@@ -1,52 +1,165 @@
+import { Address, Hex, parseAbi } from "viem";
 import {
 	AuthenticationResponseJSON,
 	PublicKeyCredentialCreationOptionsJSON,
 	PublicKeyCredentialRequestOptionsJSON,
 	RegistrationResponseJSON,
-} from './passkey.types'
+} from "./passkey.types";
+import { uint8ArrayToBase64URLString } from "./utils/encoding";
+import { Base64URLString } from "./utils/webauthn-zod";
+
+type PasskeyParams = Pick<PublicKeyCredentialCreationOptionsJSON, "rp" | "authenticatorSelection"> &
+	Partial<Pick<PublicKeyCredentialCreationOptionsJSON, "pubKeyCredParams">>;
 
 /**
  * A generic type to emulate the json-ified result of the `get` function on the browser `navigator.credential` api
  * or the result `passkey` api from `rn-passkeys`
  */
 export abstract class Passkey {
-	rp: PublicKeyCredentialCreationOptionsJSON['rp']
-
 	/**
 	 * This is simply the most widely supported public key type for webauthn
 	 * so we adopt it as a default to ease the boiler plate for the end user
 	 */
-	pubKeyCredParams: PublicKeyCredentialCreationOptionsJSON['pubKeyCredParams'] = [
-		{ type: 'public-key', alg: -7 },
-	]
+	pubKeyCredParams: PublicKeyCredentialCreationOptionsJSON["pubKeyCredParams"] = [
+		{ type: "public-key", alg: -7 },
+	];
 
 	/**
 	 * These are the default selector options for a passkey vs a 'regular' webauthn credential
 	 */
-	authenticatorSelection: PublicKeyCredentialCreationOptionsJSON['authenticatorSelection'] = {
-		residentKey: 'required',
-		userVerification: 'preferred',
-	}
+	authenticatorSelection: PublicKeyCredentialCreationOptionsJSON["authenticatorSelection"] = {
+		residentKey: "required",
+		userVerification: "preferred",
+	};
 
-	constructor(
-		params: Pick<PublicKeyCredentialCreationOptionsJSON, 'rp' | 'authenticatorSelection'> &
-			Partial<Pick<PublicKeyCredentialCreationOptionsJSON, 'pubKeyCredParams'>>,
-	) {
-		this.rp = params.rp
-		if (params.pubKeyCredParams) this.pubKeyCredParams = params.pubKeyCredParams
+	constructor(public params: PasskeyParams) {
 		if (params.authenticatorSelection)
 			this.authenticatorSelection = {
 				...this.authenticatorSelection,
 				...params.authenticatorSelection,
-			}
+			};
 	}
 
 	abstract create(
-		options: Omit<PublicKeyCredentialCreationOptionsJSON, 'rp' | 'pubKeyCredParams'> &
-			Partial<Pick<PublicKeyCredentialCreationOptionsJSON, 'pubKeyCredParams'>>,
-	): Promise<RegistrationResponseJSON | null>
+		options: Omit<PublicKeyCredentialCreationOptionsJSON, "rp" | "pubKeyCredParams"> &
+			Partial<Pick<PublicKeyCredentialCreationOptionsJSON, "pubKeyCredParams">>,
+	): Promise<RegistrationResponseJSON | null>;
 
 	abstract get(
-		options: Omit<PublicKeyCredentialRequestOptionsJSON, 'rpId'>,
-	): Promise<AuthenticationResponseJSON | null>
+		options: Omit<PublicKeyCredentialRequestOptionsJSON, "rpId">,
+	): Promise<AuthenticationResponseJSON | null>;
+}
+
+interface R1SignatureResult {
+	r: Base64URLString;
+	s: Base64URLString;
+	rawSignature: Base64URLString;
+	authenticatorDataHex: Hex;
+	clientDataJSONHex: Hex;
+}
+
+interface CounterFactualSmartAccountArgs {
+	/**
+	 * This is the salt that will be used when deploying the account
+	 */
+	salt?: bigint;
+	factory: {
+		/**
+		 * The abi of the smart account or its deploy function
+		 * should be in a form that is parsable by viem [`parseAbi`](https://viem.sh/docs/abi/parseAbi.html#parseabi)
+		 */
+		deployFunctionAbi: readonly string[];
+		deployFunctionName: string;
+		/**
+		 * The accounts factory address
+		 */
+		address: Address;
+	};
+
+	// /**
+	//  * This is the init code needed to deploy the account
+	//  */
+	// initCode: Hex;
+}
+
+interface DeployedSmartAccountArgs {
+	/**
+	 * The deployed smart account address
+	 */
+	address: Address;
+}
+
+interface BaseSmartAccountPasskeyParams {
+	/**
+	 * This is the credentialId of the passkey used to identify the correct passkey
+	 */
+	credentialId: Base64URLString;
+	/**
+	 * The accounts supported entrypoint
+	 */
+	entrypoint: Address;
+}
+
+type SmartAccountPasskeyParams = BaseSmartAccountPasskeyParams &
+	(CounterFactualSmartAccountArgs | DeployedSmartAccountArgs);
+
+export abstract class SmartAccountPasskey extends Passkey {
+	constructor(public account: SmartAccountPasskeyParams, public params: PasskeyParams) {
+		super(params);
+	}
+
+	assertsIsDeployed(): asserts this is Extract<
+		this,
+		{ account: { smartAccount: DeployedSmartAccountArgs } }
+	> {
+		// TODO: With the initCode we could actually assign the counterfactual address if not deployed
+		if ("factory" in this.account) throw new Error("This account has not been deployed");
+	}
+
+	/**
+	 * @description A static helper method to make it easier to handle converting the signature result of a passkey in to
+	 * something that is more easily passed onchain
+	 *
+	 * @param signature The resulting signature from a passkey verification in the standard ASN.1 format encoded as base64url
+	 *
+	 * @example ```ts
+	 * const allowCredentials = [{ type: 'public-key', credentialId }]
+	 * const passkeyResult = await this.get({ challenge, allowCredentials })
+	 * const base64UrlSignature = passkeyResult.response.signature
+	 * const { rawSignature, r, s } = this.getRAndSFromSignature(base64UrlSignature)
+	 * ```
+	 */
+	static getRAndSFromSignature(signature: Base64URLString) {
+		const signatureBytes = Buffer.from(signature);
+		const usignature = new Uint8Array(signatureBytes);
+		const rStart = usignature[4] === 0 ? 5 : 4;
+		const rEnd = rStart + 32;
+		const sStart = usignature[rEnd + 2] === 0 ? rEnd + 3 : rEnd + 2;
+		const r = usignature.slice(rStart, rEnd);
+		const s = usignature.slice(sStart);
+
+		const rawSignature = new Uint8Array([...r, ...s]);
+
+		return {
+			rawSignature: uint8ArrayToBase64URLString(rawSignature),
+			r: uint8ArrayToBase64URLString(r),
+			s: uint8ArrayToBase64URLString(s),
+		} satisfies Pick<R1SignatureResult, "r" | "rawSignature" | "s">;
+	}
+
+	/**
+	 * @description If the smart account uses a passkey as the signer (i.e. verifies with secp256r1 onchain) then this
+	 * method is what is used to sign the transaction.
+	 *
+	 * **Your method implementation should handle all the normal server verification involved with using webauthn**
+	 *
+	 * @param messageBytes The message to be signed in base64url format
+	 * @param credentialId The credentialId of the passkey to sign with in base64url format
+	 * @returns `R1SignatureResult` {@link R1SignatureResult} The signature and other data associated with the passkey verification result
+	 * @throws {Error}
+	 */
+	abstract signR1(
+		messageBytes: Base64URLString,
+		credentialId: Base64URLString,
+	): Promise<R1SignatureResult | null>;
 }
--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -1,6 +1,7 @@
-import type { Hex } from 'viem'
+import type { Hex } from "viem";
+import type { Base64URLString } from "./webauthn-zod";
 
-// ! taken from https://github.com/MasterKale/SimpleWebAuthn/blob/e02dce6f2f83d8923f3a549f84e0b7b3d44fa3da/packages/browser/src/helpers/base64URLStringToBuffer.ts
+// ! modified from https://github.com/MasterKale/SimpleWebAuthn/blob/e02dce6f2f83d8923f3a549f84e0b7b3d44fa3da/packages/browser/src/helpers/base64URLStringToBuffer.ts
 /**
  * Convert from a Base64URL-encoded string to an Array Buffer. Best used when converting a
  * credential ID from a JSON string to an ArrayBuffer, like in allowCredentials or
@@ -10,7 +11,7 @@ import type { Hex } from 'viem'
  */
 export function base64URLStringToBuffer(base64URLString: string): ArrayBuffer {
 	// Convert from Base64URL to Base64
-	const base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/')
+	const base64 = base64URLString.replace(/-/g, "+").replace(/_/g, "/");
 	/**
 	 * Pad with '=' until it's a multiple of four
 	 * (4 - (85 % 4 = 1) = 3) % 4 = 3 padding
@@ -18,41 +19,46 @@ export function base64URLStringToBuffer(base64URLString: string): ArrayBuffer {
 	 * (4 - (87 % 4 = 3) = 1) % 4 = 1 padding
 	 * (4 - (88 % 4 = 0) = 4) % 4 = 0 padding
 	 */
-	const padLength = (4 - (base64.length % 4)) % 4
-	const padded = base64.padEnd(base64.length + padLength, '=')
+	const padLength = (4 - (base64.length % 4)) % 4;
+	const padded = base64.padEnd(base64.length + padLength, "=");
 
 	// Convert to a binary string
-	const binary = atob(padded)
+	const binary = atob(padded);
 
 	// Convert binary string to buffer
-	const buffer = new ArrayBuffer(binary.length)
-	const bytes = new Uint8Array(buffer)
+	const buffer = new ArrayBuffer(binary.length);
+	const bytes = new Uint8Array(buffer);
 
 	for (let i = 0; i < binary.length; i++) {
-		bytes[i] = binary.charCodeAt(i)
+		bytes[i] = binary.charCodeAt(i);
 	}
 
-	return buffer
+	return buffer;
 }
 
-// ! taken from https://github.com/MasterKale/SimpleWebAuthn/blob/e02dce6f2f83d8923f3a549f84e0b7b3d44fa3da/packages/browser/src/helpers/bufferToBase64URLString.ts
+// ! modified from https://github.com/MasterKale/SimpleWebAuthn/blob/e02dce6f2f83d8923f3a549f84e0b7b3d44fa3da/packages/browser/src/helpers/bufferToBase64URLString.ts
 /**
  * Convert the given array buffer into a Base64URL-encoded string. Ideal for converting various
  * credential response ArrayBuffers to string for sending back to the server as JSON.
  *
  * Helper method to compliment `base64URLStringToBuffer`
  */
-export function bufferToBase64URLString(buffer: ArrayBuffer): string {
-	const bytes = new Uint8Array(buffer)
-	let str = ''
+export function bufferToBase64URLString(buffer: ArrayBuffer): Base64URLString {
+	const bytes = new Uint8Array(buffer);
+
+	return uint8ArrayToBase64URLString(bytes);
+}
+
+export function uint8ArrayToBase64URLString(bytes: Uint8Array): Base64URLString {
+	let str = "";
 
 	for (const charCode of bytes) {
-		str += String.fromCharCode(charCode)
+		str += String.fromCharCode(charCode);
 	}
 
-	const base64String = btoa(str)
+	const base64String = btoa(str);
 
-	return base64String.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+	return base64String.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "") as Base64URLString;
 }
 
 export function hexToBuffer(hexString: Hex): ArrayBuffer {
@@ -60,18 +66,18 @@ export function hexToBuffer(hexString: Hex): ArrayBuffer {
 		hexString
 			.slice(2)
 			.match(/.{1,2}/g)
-			?.map((byte) => parseInt(byte, 16)) ?? []
-	return Uint8Array.from(bytes)
+			?.map((byte) => parseInt(byte, 16)) ?? [];
+	return Uint8Array.from(bytes);
 }
 
 export function bufferToHex(buffer: ArrayBuffer): Hex {
-	return `0x${[...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, '0')).join('')}`
+	return `0x${[...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, "0")).join("")}`;
 }
 
 export function base64URLStringToHex(base64URLString: string): Hex {
-	return bufferToHex(base64URLStringToBuffer(base64URLString))
+	return bufferToHex(base64URLStringToBuffer(base64URLString));
 }
 
 export function hexToBase64URLString(hexString: Hex): string {
-	return bufferToBase64URLString(hexToBuffer(hexString))
+	return bufferToBase64URLString(hexToBuffer(hexString));
 }
