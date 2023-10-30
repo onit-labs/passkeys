a1cc19415a007ca0717ef53c916851b91fafcd1c
Peter Ferguson <peterferguson95@gmail.com>
2023-10-30 14:02:14 +0000

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 export * from "./large-blob-passkey-account";
 export * from "./passkey";
-// export * from "./passkey-eoa-connector";
 export * from "./passkey-4337-connector";
-export * from "./passkey.types";
-
-export {
-	type Base64String,
-	type Base64URLString,
-	webauthnAuthenticationResponseSchema,
-	webauthnRegistrationResponseSchema as webauthnRegistrationResultSchema,
-} from "./webauthn-zod";
+export * from "./webauthn-zod";
+export * as utils from "./utils/encoding";
